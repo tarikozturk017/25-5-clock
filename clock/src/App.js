9929c2f1abe5e89a1b1ad6cf080b1322aa5c4b24
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-12-01 23:19:00 -0500

    basic setup

--- clock/src/App.js
+++ clock/src/App.js
@@ -16,9 +16,14 @@ function App() {
       <button onClick={() => handleClick()} id={"break-increment"}></button>
       <button onClick={() => handleClick()} id={"session-increment"}></button>
 
-      <div id={'break-length'} >5</div>
-      <div id={'session-length'} >25</div>
+      <div id={"break-length"} >5</div>
+      <div id={"session-length"} >25</div>
       
+      <div id={"timer-label"} >Session</div>
+      <div id={"time-left"} >25:00</div>
+
+      <button onClick={() => handleClick()} id={"start_stop"}></button>
+      <button onClick={() => handleClick()} id={"reset"}></button>
     </div>
   );
 }
